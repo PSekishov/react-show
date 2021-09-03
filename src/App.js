c9bb9c9c25322a55d8e7e7e2eb2c7ad076d801b6
Pavel Sekishov <pasha-sveta@yandex.ru>
2021-09-04 02:59:05 +0300

    2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Pavel Sekishov
+         Pavel Sekishov Test React
         </a>
       </header>
     </div>
